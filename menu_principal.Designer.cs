81300fb3ce8dcedee0b54658fb49675bc345a0ac
salvadoreandradep <salvadorplay7089@gmail.com>
2022-08-27 22:51:50 -0600

    0.0.0.1

--- menu_principal.Designer.cs
+++ menu_principal.Designer.cs
@@ -44,9 +44,9 @@ namespace Semana_5_y_6
             this.pastelesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tartaletasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.facturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.acerdaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,7 +57,6 @@ namespace Semana_5_y_6
             this.ensaladasToolStripMenuItem,
             this.postresToolStripMenuItem,
             this.facturaToolStripMenuItem,
-            this.acerdaDeToolStripMenuItem,
             this.ayudaToolStripMenuItem,
             this.salirToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
@@ -82,35 +81,35 @@ namespace Semana_5_y_6
             // combo1ToolStripMenuItem
             // 
             this.combo1ToolStripMenuItem.Name = "combo1ToolStripMenuItem";
-            this.combo1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.combo1ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
             this.combo1ToolStripMenuItem.Text = "Pagina de Combos";
             this.combo1ToolStripMenuItem.Click += new System.EventHandler(this.combo1ToolStripMenuItem_Click);
             // 
             // combo2ToolStripMenuItem
             // 
             this.combo2ToolStripMenuItem.Name = "combo2ToolStripMenuItem";
-            this.combo2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.combo2ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
             this.combo2ToolStripMenuItem.Text = "Combo 1";
             this.combo2ToolStripMenuItem.Click += new System.EventHandler(this.combo2ToolStripMenuItem_Click);
             // 
             // combo3ToolStripMenuItem
             // 
             this.combo3ToolStripMenuItem.Name = "combo3ToolStripMenuItem";
-            this.combo3ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.combo3ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
             this.combo3ToolStripMenuItem.Text = "Combo 2";
             this.combo3ToolStripMenuItem.Click += new System.EventHandler(this.combo3ToolStripMenuItem_Click);
             // 
             // combo3ToolStripMenuItem1
             // 
             this.combo3ToolStripMenuItem1.Name = "combo3ToolStripMenuItem1";
-            this.combo3ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.combo3ToolStripMenuItem1.Size = new System.Drawing.Size(174, 22);
             this.combo3ToolStripMenuItem1.Text = "Combo 3";
             this.combo3ToolStripMenuItem1.Click += new System.EventHandler(this.combo3ToolStripMenuItem1_Click);
             // 
             // combo4ToolStripMenuItem
             // 
             this.combo4ToolStripMenuItem.Name = "combo4ToolStripMenuItem";
-            this.combo4ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.combo4ToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
             this.combo4ToolStripMenuItem.Text = "Combo 4";
             this.combo4ToolStripMenuItem.Click += new System.EventHandler(this.combo4ToolStripMenuItem_Click);
             // 
@@ -126,7 +125,7 @@ namespace Semana_5_y_6
             // sinExsitenciasToolStripMenuItem
             // 
             this.sinExsitenciasToolStripMenuItem.Name = "sinExsitenciasToolStripMenuItem";
-            this.sinExsitenciasToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.sinExsitenciasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.sinExsitenciasToolStripMenuItem.Text = "Bocadillos";
             // 
             // postresToolStripMenuItem
@@ -142,19 +141,19 @@ namespace Semana_5_y_6
             // heladoToolStripMenuItem
             // 
             this.heladoToolStripMenuItem.Name = "heladoToolStripMenuItem";
-            this.heladoToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.heladoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.heladoToolStripMenuItem.Text = "Helado";
             // 
             // pastelesToolStripMenuItem
             // 
             this.pastelesToolStripMenuItem.Name = "pastelesToolStripMenuItem";
-            this.pastelesToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.pastelesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.pastelesToolStripMenuItem.Text = "Pasteles";
             // 
             // tartaletasToolStripMenuItem
             // 
             this.tartaletasToolStripMenuItem.Name = "tartaletasToolStripMenuItem";
-            this.tartaletasToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.tartaletasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.tartaletasToolStripMenuItem.Text = "Tartaletas";
             // 
             // facturaToolStripMenuItem
@@ -163,14 +162,10 @@ namespace Semana_5_y_6
             this.facturaToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
             this.facturaToolStripMenuItem.Text = "Factura";
             // 
-            // acerdaDeToolStripMenuItem
-            // 
-            this.acerdaDeToolStripMenuItem.Name = "acerdaDeToolStripMenuItem";
-            this.acerdaDeToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
-            this.acerdaDeToolStripMenuItem.Text = "Acerda de";
-            // 
             // ayudaToolStripMenuItem
             // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem});
             this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
             this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.ayudaToolStripMenuItem.Text = "Ayuda";
@@ -181,6 +176,12 @@ namespace Semana_5_y_6
             this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
             // 
             // menu_principal
             // 
@@ -214,11 +215,11 @@ namespace Semana_5_y_6
         private System.Windows.Forms.ToolStripMenuItem pastelesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem tartaletasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem facturaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem acerdaDeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem combo3ToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem combo4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
     }
 }
 
