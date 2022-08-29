205a521bd3727f450f7cd9a10cd76a953b926da0
DAVID ERNESTO JAIME CARRILLO <USSS105322@ugb.edu.sv>
2022-08-28 19:26:53 -0600

    Cambio de colores

--- helados.Designer.cs
+++ helados.Designer.cs
@@ -93,7 +93,7 @@ namespace Semana_5_y_6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(778, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
